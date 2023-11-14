b9275926b4a6b26bf876da225278a3c1efc976ec
Erin Wolff <erin_wolff@outlook.com>
2023-11-14 13:30:24 -0800

    bts completed

--- src/bst.js
+++ src/bst.js
@@ -1,5 +1,44 @@
 class BinarySearchTree {
-  // YOUR CODE HERE
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.magnitude = 1;
+  }
+
+  insert(value) {
+    if (value === this.value) {
+      return;
+    }
+    const direction = value < this.value ? 'left' : 'right'
+    if (this[direction]) {
+      this[direction].insert(value)
+    } else {
+      this[direction] = new BinarySearchTree(value)
+    }
+    this.magnitude++
+  }
+
+  size() {
+    return this.magnitude;
+  }
+
+  contains(value) {
+    if (this.value === value) {
+      return true
+    }
+    const direction = value < this.value ? 'left' : 'right'
+    if (this[direction]) {
+      return this[direction].contains(value)
+    } else {
+      return false;
+    }
+  }
+
+  depthFirstForEach() { }
+  breadthFirstForEach() { }
+
+
 }
 
 module.exports = BinarySearchTree
